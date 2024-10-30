c287baec38932ed5042a0460cca63b11c5ad5778
Zafar Ahmed <zafarahmed2k24@gmail.com>
2024-10-30 16:23:27 +0530

    fix: type correct in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,11 @@ export default function Footer() {
     <footer className="bg-transparent">
       <div className="px-4 py-4 mx-auto text-xs text-center text-gray-500 max-w-7xl sm:px-6 lg:px-8">
         Copyright 2024 - Paragon Testing Enterprises |{" "}
-        <span className="text-customBlue"> www.HZad Education.ca </span> |{" "}
-        <span className="text-customBlue"> Contact Us </span>
+        <a href="www.hzadeducation.com " className="text-customBlue">
+          {" "}
+          www.hzadeducation.com{" "}
+        </a>{" "}
+        | <span className="text-customBlue"> Contact Us </span>
       </div>
     </footer>
   );
